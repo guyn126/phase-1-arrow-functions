833bf3df320c156a957559441958e8d5e5548753
student.moringaschool.com <guy.nkurunziza@student.moringaschool.com>
2025-03-15 11:57:11 +0200

    Arrow functions

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function (a, b) {
+  return a / b;
+};
+
+const square = (x) => x * x;
+
+const add = (a, b) => a + b;
+
+console.log(divide(2000, 100));
+console.log(square(5));
+console.log(add(10, 5));
